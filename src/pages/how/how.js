b8605be50c5fa1bac7dc383707e7250900208b90
Nataliia_Matviienko <92440177+Company-Smart-ui@users.noreply.github.com>
2022-12-16 21:51:36 +0200

    started page How

--- src/pages/how/how.js
+++ src/pages/how/how.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from './how.module.scss'
+import {Title} from "../../components/title";
+import {HowWorksCard} from "./howWorksCard/howWorksCard";
 
+import imgMobile from "./img-mobile-how.png";
 export const How = () => {
+    const dataHow = window.dataHow;
 
     return <div className={styles.how}>
-how
+            <div className="container">
+                <Title title={dataHow.title}/>
+
+                <div className={styles.wrapper}>
+                    <div className={styles.wrapImg}>
+                        <img src={imgMobile} alt="imgMobile"/>
+                    </div>
+
+                    <div className={styles.wrapHowWorksCard}>
+                        { dataHow.howWorksCard?.map((item, id) => {
+                            return <HowWorksCard className={styles.howWorksCard} key={id} props={item}/>
+                        })}
+                    </div>
+                </div>
+                <div className={styles.wrapper}>
+
+                </div>
+            </div>
     </div>
 };
  
