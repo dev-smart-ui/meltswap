ae035ba4ee1d42a156b0781ac7f7e7ea6aa11ccf
Viktor <vmoskalev@smart-ui.pro>
2022-12-16 23:04:19 +0200

    add tab

--- src/pages/home/price/price.js
+++ src/pages/home/price/price.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import styles from './price.module.scss'
+import {Tabs} from "../../../components/tabs/tabs";
 
+
+const tab1=()=> <h2> tab1</h2>
+const tab2=()=> <h2> tab2</h2>
+const tab3=()=> <h2> tab3</h2>
+
+const tabList = [
+    {  anchor:"Top Cryptocurrencies",  element :tab1   },
+    {  anchor:"Top Gainers",  element :tab2   },
+    {  anchor:"Top Losers",  element :tab3   },
+]
 
 export const Price = () => {
     return <section className={styles.price}>
         <div className='container'>
             <h2 className={styles.title}>Price charts</h2>
-            
+            <Tabs tabList={tabList}/>
             <a href='/' className='btn'>More assets</a>
         </div>
     </section>
