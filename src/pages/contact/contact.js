3357d73fca4bda2ab7af8f509b8affa30af3cd88
Vlad <vtshenko@smart-ui.pro>
2022-12-19 01:12:22 +0200

    added a contact page and tab content on the home page

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,11 +1,80 @@
 import React from 'react';
 import styles from './contact.module.scss'
+import {Title} from "../../components/title";
+import email from "./img/email.svg";
+import adress from "./img/adress.svg";
+import logo from "./img/contact-logo.svg";
+import { useState } from 'react';
 
 export const Contact = () => {
+    const dataContact = window.dataContact;
 
-    return <div className={styles.contact}>
-        contact
+    const [inputs, setInputs] = useState({});
 
-    </div>
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    }
+
+    return <section className={styles.contact}>
+        <Title title={dataContact.title} subtitle={dataContact.subtitle}/>
+        <div className={styles.wrap}>
+            <div className={styles.left}>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.input}>
+                        <label>Full Name</label>
+                        <input type='text' name='name' value={inputs.name || ""} placeholder="Your name" className={styles.field} onChange={handleChange}></input>
+                    </div>
+                    <div className={styles.input}>
+                        <label>Email</label>
+                        <input type='text' name='email' value={inputs.email || ""} placeholder="yourname@company.com" className={styles.field} onChange={handleChange}></input>
+                    </div>
+                    <div className={styles.input}>
+                        <label>How can we help?</label>
+                        <textarea type='text' name='message' value={inputs.message || ""} placeholder="Your message"  className={styles.field} onChange={handleChange}></textarea>
+                    </div>
+                    
+                    <button type='submit' onChange={handleSubmit} className="btn">Submit</button>
+                </form>
+            </div>
+            <div className={styles.right}>
+                <div className={styles.block}>
+                    <div className={styles.icon}>
+                        <img src={email} alt="Email" />
+                    </div>
+                    <div className={styles.info}>
+                        <div className={styles.infoTitle}>
+                            Email
+                        </div>
+                        <a href='mailto:support@meltswap.com' className={styles.infoEmail}>
+                            support@meltswap.com
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.block}>
+                    <diiv className={styles.icon}>
+                        <img src={adress} alt="Adress" />
+                    </diiv>
+                    <diiv className={styles.info}>
+                        <div className={styles.infoTitle}>
+                            Address
+                        </div>
+                        <div className={styles.infoText}>
+                            Spaces V&A Waterfront <br/>
+                            Dock Road Junction <br/>
+                            Cnr of Dock Stanley Road Waterfront <br/>
+                            Cape town 8001
+                        </div>
+                    </diiv>
+                </div>
+                <img src={logo} alt="Logo" className={styles.img} />
+            </div>
+        </div>
+    </section>
 };
  
--- src/pages/home/price/price.js
+++ src/pages/home/price/price.js
@@ -1,9 +1,74 @@
 import React from 'react';
 import styles from './price.module.scss'
+import bitcoin from './img/bitcoin.svg'
 import {Tabs} from "../../../components/tabs/tabs";
 
 
-const tab1=()=> <h2> tab1</h2>
+const tab1=()=> { return <div>
+<div className={styles.tabsBlock}>
+    <div className={styles.numberBlock}>
+        #
+    </div>
+    <div className={styles.currencyBlock}>
+        Currency
+    </div>
+    <div className={styles.priceBlock}>
+        Price
+    </div>
+    <div className={styles.changeBlock}>
+        24 Change
+    </div>
+    <div className={styles.marketBlock}>
+        Market Cap
+    </div>
+    <div>
+
+    </div>
+</div>
+<div>
+    <div className={styles.tabsContent}>
+        <div className={styles.numb}>
+            1
+        </div>
+        <div className={styles.currency}>
+            <img src={bitcoin} alt="Bitcoin" /> <div className={styles.currencyTitle}><span>BTC</span> Bitcoin</div> 
+        </div>
+        <div className={styles.value}>
+            $32456.78
+        </div>
+        <div className={styles.change}>
+            4.58%
+        </div>
+        <div className={styles.market}>
+            $234,123,567,367
+        </div>
+        <div className={styles.btnExchange}>
+            <button type='button'>Exchange</button>
+        </div>
+    </div>
+    <div className={styles.tabsContent}>
+        <div className={styles.numb}>
+            1
+        </div>
+        <div className={styles.currency}>
+            <img src={bitcoin} alt="Bitcoin" /> <div className={styles.currencyTitle}><span>BTC</span> Bitcoin</div> 
+        </div>
+        <div className={styles.value}>
+            $32456.78
+        </div>
+        <div className={styles.change}>
+            4.58%
+        </div>
+        <div className={styles.market}>
+            $234,123,567,367
+        </div>
+        <div className={styles.btnExchange}>
+            <button type='button'>Exchange</button>
+        </div>
+    </div>
+</div>
+</div>
+};
 const tab2=()=> <h2> tab2</h2>
 const tab3=()=> <h2> tab3</h2>
 
