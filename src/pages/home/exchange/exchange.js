01b2f49cef2ba9cbbea2e41fb14bd05f69772e49
Nataliia_Matviienko <92440177+Company-Smart-ui@users.noreply.github.com>
2022-12-21 15:33:52 +0200

    merge about page

--- src/pages/home/exchange/exchange.js
+++ src/pages/home/exchange/exchange.js
@@ -4,14 +4,16 @@ import exchanges from './img/Exchange.svg';
 
 
 export const Exchange = () => {
-
+    const sizePaddingBottom = ((393 / 491)  * 100);  // (( height / width)  * 100)
     return <section className={styles.exchange}>
         <div className={styles.content}>
             <h1> <span>Trusted</span> & <span>secure</span> platform for crypto exchange </h1>
             <p>Get started with the easiest and most secure platform to exchange cryptocurrencies</p>
         </div>
         <div className={styles.img}>
-            <img src={exchanges} alt='Exchange' />
+            <figure className={"figure-webp"}  style={{paddingBottom: `${sizePaddingBottom}%`}}>
+                <img src={exchanges} alt='Exchange' />
+            </figure>
         </div>
     </section>
 };
