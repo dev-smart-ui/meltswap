6ce9c163d61272d86752dffb9b9af0e2afbebf0d
Vlad <vtshenko@smart-ui.pro>
2023-01-26 14:18:12 +0200

    change format price

--- src/components/table/block/Price/tablePriceMobile.js
+++ src/components/table/block/Price/tablePriceMobile.js
@@ -6,7 +6,7 @@ export const TablePriceMobile = (props) => {
         
     return (<>
         <div className={styles.value}>
-            ${Intl.NumberFormat().format(price)} 
+            ${Intl.NumberFormat('en-IN').format(price)} 
         </div>
         <div className={styles.change}>
             {props.data.change}
